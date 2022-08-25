0b8de5c10eb63ef8107d9a043803eb2b797641cb
vipku <vip.kufaev@mail.ru>
2022-08-25 21:16:25 +0500

    Add Sort

--- src/ArrList.java
+++ src/ArrList.java
@@ -1,9 +1,17 @@
-public interface ArrList<T> {
+public interface ArrList<T> extends Iterable<T>{
 
     void add(T element);
 
     void add(T element, int index);
 
     void delete(T element);
+
+    void mergeSort(T[] array, int start, int finish);
+
+    T[] get();
+
+    int length();
+
+    T get(int index);
 
 }
--- src/ArrListImpl.java
+++ src/ArrListImpl.java
@@ -1,12 +1,14 @@
-public class ArrListImpl<T> implements ArrList<T>{
+import java.util.Iterator;
 
-    private T[] elements = (T[]) new Object[12];
+public class ArrListImpl<T extends Comparable<? super T>> implements ArrList<T> {
+
+    private T[] elements = (T[]) new Comparable[12];
     private int size = 0;
 
     @Override
     public void add(T element) {
         if (size >= elements.length) {
-            T[] newArray = (T[]) new Object[elements.length * 2];
+            T[] newArray = (T[]) new Comparable[elements.length * 2];
             for (int i = 0; i < elements.length; i++) {
                 newArray = elements;
             }
@@ -18,11 +20,11 @@ public class ArrListImpl<T> implements ArrList<T>{
 
     @Override
     public void add(T element, int index) {
-       for (int i = size; i > index; i--) {
-           elements[i] = elements[i - 1];
-       }
-       elements[index] = element;
-       size++;
+        for (int i = size; i > index; i--) {
+            elements[i] = elements[i - 1];
+        }
+        elements[index] = element;
+        size++;
     }
 
     @Override
@@ -36,4 +38,73 @@ public class ArrListImpl<T> implements ArrList<T>{
             }
         }
     }
+
+    @Override
+    public T[] get() {
+        return elements;
+    }
+
+    @Override
+    public int length() {
+        return elements.length;
+    }
+
+    @Override
+    public void mergeSort(T[] array, int start, int end)
+    {
+        if (start < end)
+        {
+            int middle = (start + end) / 2;
+            mergeSort(array, start, middle);
+            mergeSort(array, middle + 1, end);
+            merge(array, start, middle, end);
+        }
+    }
+
+    public void merge(T[] array, int start, int middle, int end)
+    {
+        T[] leftArray  = (T[]) new Comparable[middle - start + 1];
+        T[] rightArray = (T[]) new Comparable[end - middle];
+
+        for (int i = 0; i < leftArray.length; ++i)
+            leftArray[i] = array[start + i];
+
+        for (int i = 0; i < rightArray.length; ++i)
+            rightArray[i] = array[middle + 1 + i];
+
+        int leftIndex = 0, rightIndex = 0;
+        int currentIndex = start;
+        while (leftIndex < leftArray.length && rightIndex < rightArray.length)
+        {
+            if (leftArray[leftIndex].compareTo(rightArray[rightIndex]) <= 0)
+            {
+                array[currentIndex] = leftArray[leftIndex];
+                leftIndex++;
+            }
+            else
+            {
+                array[currentIndex] = rightArray[rightIndex];
+                rightIndex++;
+            }
+            currentIndex++;
+        }
+
+
+        while (leftIndex < leftArray.length) array[currentIndex++] = leftArray[leftIndex++];
+
+        while (rightIndex < rightArray.length) array[currentIndex++] = rightArray[rightIndex++];
+    }
+
+    @Override
+    public T get(int index) {
+        return (T) elements[index];
+    }
+
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ArrayItertor<>(elements);
+    }
 }
+
+
